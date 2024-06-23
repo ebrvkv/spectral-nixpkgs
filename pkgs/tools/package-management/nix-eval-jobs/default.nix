24a89b9409bc7e1d99b1cb2885ffc8a2d1639899
Kirill Boltaev <kirill.b@spectral-team.com>
2024-06-24 03:58:50 +0700

    Use 2.20.6 nix-eval-jobs

--- pkgs/tools/package-management/nix-eval-jobs/default.nix
+++ pkgs/tools/package-management/nix-eval-jobs/default.nix
@@ -11,12 +11,12 @@
 }:
 stdenv.mkDerivation rec {
   pname = "nix-eval-jobs";
-  version = "2.21.0";
+  version = "2.20.6";
   src = fetchFromGitHub {
-    owner = "nix-community";
+    owner = "spectral-team";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-StJq7Zy+/iVBUAKFzhHWlsirFucZ3gNtzXhAYXAsNnw=";
+    hash = "sha256-aqbiCTeeiYX7eYdC7TNieYGNWyfh1kqYOXR/PeofQf0=";
   };
   buildInputs = [
     boost
