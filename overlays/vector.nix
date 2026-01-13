190182339b20ebe6b14ae09e678e1b0f2e574d5b
Vladislav Arefev <vlad.a@spectral-team.com>
2026-01-13 15:55:08 +0000

    fix: delete vrl

--- overlays/vector.nix
+++ overlays/vector.nix
@@ -4,7 +4,6 @@ let
   rp = final.makeRustPlatform { rustc = tc; cargo = tc; };
 in
 {
-  vrl = prev.vrl.override { rustPlatform = rp; };
   vector = prev.vector.override {
     rustPlatform = rp;
     auditable = false;
